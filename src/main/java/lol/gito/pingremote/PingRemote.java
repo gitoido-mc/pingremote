18700e6f7009612931ed6c4fa6f97d47235a7c5c
gitoido <warzgibz@gmail.com>
2025-05-30 18:33:30 +0700

    feat: ping remote host

--- src/main/java/lol/gito/pingremote/PingRemote.java
+++ src/main/java/lol/gito/pingremote/PingRemote.java
@@ -8,25 +8,50 @@ import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+
 public class PingRemote implements ModInitializer {
-	public static final String MOD_ID = "pingremote";
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-	public static PingRemoteConfig config;
-	public static int counter;
+    public static final String MOD_ID = "pingremote";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    public static PingRemoteConfig config;
+    public static int counter;
 
-	@Override
-	public void onInitialize() {
-		LOGGER.info("Ping Remote initialized.");
-		config = ConfigBuilder.load(PingRemoteConfig.class, MOD_ID);
+    @Override
+    public void onInitialize() {
+        LOGGER.info("Ping Remote initialized.");
+        config = ConfigBuilder.load(PingRemoteConfig.class, MOD_ID);
 
-		ServerTickEvents.START_SERVER_TICK.register((serverWorld) -> {
-			PingRemote.counter++;
-		});
+        ServerTickEvents.START_SERVER_TICK.register((serverWorld) -> {
+            PingRemote.counter++;
+        });
 
-		ServerTickEvents.END_WORLD_TICK.register((serverWorld) -> {
-			if (PingRemote.counter == PingRemote.config.getTickAmount()) {
+        ServerTickEvents.END_WORLD_TICK.register((serverWorld) -> {
+            if (counter == config.getTickAmount()) {
+                counter = 0;
 
-			}
-		});
-	}
+                try (HttpClient client = HttpClient.newBuilder()
+                        .version(HttpClient.Version.HTTP_2)
+                        .connectTimeout(Duration.ofSeconds(10))
+                        .build()) {
+
+                    try {
+                        HttpRequest request = HttpRequest.newBuilder()
+                                .uri(config.getRemoteHost().toURI())
+                                .timeout(Duration.ofMinutes(1))
+                                .build();
+
+                        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                                .thenApply(HttpResponse::body)
+                                .thenAccept(System.out::println);
+                    } catch (URISyntaxException e) {
+                        LOGGER.warn("Cannot ping remote host");
+                    }
+                }
+            }
+        });
+    }
 }
