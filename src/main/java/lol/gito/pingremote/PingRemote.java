d812e103394ef686a72e448bd56fdf7fcd02738e
gitoido <warzgibz@gmail.com>
2025-07-02 14:48:01 +0700

    feat: default host, debug config

--- src/main/java/lol/gito/pingremote/PingRemote.java
+++ src/main/java/lol/gito/pingremote/PingRemote.java
@@ -25,33 +25,43 @@ public class PingRemote implements ModInitializer {
         LOGGER.info("Ping Remote initialized.");
         config = ConfigBuilder.load(PingRemoteConfig.class, MOD_ID);
 
-        ServerTickEvents.START_SERVER_TICK.register((serverWorld) -> {
-            PingRemote.counter++;
-        });
+        if (config.getRemoteHost() != null) {
+            ServerTickEvents.START_SERVER_TICK.register((serverWorld) -> {
+                PingRemote.counter++;
+            });
 
-        ServerTickEvents.END_WORLD_TICK.register((serverWorld) -> {
-            if (counter == config.getTickAmount()) {
-                counter = 0;
+            ServerTickEvents.END_WORLD_TICK.register((serverWorld) -> {
+                if (counter == config.getTickAmount()) {
+                    counter = 0;
 
-                try (HttpClient client = HttpClient.newBuilder()
-                        .version(HttpClient.Version.HTTP_2)
-                        .connectTimeout(Duration.ofSeconds(10))
-                        .build()) {
+                    try (HttpClient client = HttpClient.newBuilder()
+                            .version(HttpClient.Version.HTTP_2)
+                            .connectTimeout(Duration.ofSeconds(10))
+                            .build()) {
 
-                    try {
-                        HttpRequest request = HttpRequest.newBuilder()
-                                .uri(config.getRemoteHost().toURI())
-                                .timeout(Duration.ofMinutes(1))
-                                .build();
+                        try {
+                            HttpRequest request = HttpRequest.newBuilder()
+                                    .uri(config.getRemoteHost().toURI())
+                                    .timeout(Duration.ofMinutes(1))
+                                    .build();
 
-                        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
-                                .thenApply(HttpResponse::body)
-                                .thenAccept(System.out::println);
-                    } catch (URISyntaxException e) {
-                        LOGGER.warn("Cannot ping remote host");
+                            client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                                    .thenApply(HttpResponse::body)
+                                    .thenAccept((response) -> {
+                                        if (config.isDebug()) {
+                                            LOGGER.info("Response body from remote host");
+                                            LOGGER.info(response);
+                                        }
+                                    });
+                        } catch (URISyntaxException e) {
+                            LOGGER.warn("Cannot ping remote host, invalid uri");
+                        }
                     }
                 }
-            }
-        });
+            });
+        } else {
+            LOGGER.warn("Remote host is not provided");
+            LOGGER.warn("Skipping mod event registration");
+        }
     }
 }
--- src/main/java/lol/gito/pingremote/config/PingRemoteConfig.java
+++ src/main/java/lol/gito/pingremote/config/PingRemoteConfig.java
@@ -1,10 +1,16 @@
 package lol.gito.pingremote.config;
 
+import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URL;
 
 public class PingRemoteConfig {
-    private URL remoteHost;
+    private URL remoteHost = URI.create("https://example.com").toURL();
     private int tickAmount = 6000;
+    private boolean debug = false;
+
+    public PingRemoteConfig() throws MalformedURLException {
+    }
 
     public URL getRemoteHost() {
         return remoteHost;
@@ -20,5 +26,13 @@ public class PingRemoteConfig {
 
     public void setTickAmount(int tickAmount) {
         this.tickAmount = tickAmount;
+    }
+
+    public boolean isDebug() {
+        return debug;
+    }
+
+    public void setDebug(boolean debug) {
+        this.debug = debug;
     }
 }
